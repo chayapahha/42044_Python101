003be4bba489b6aae72e640defeac41ad63bf7e4
chayapahha <chayapatfongkham@gmail.com>
2026-06-24 09:58:42 +0700

    1-7_EZZZZ

---
+++ 03-pythonVariables.py
@@ -0,0 +1,6 @@
+team1 = "Brazil"
+team2 = "germany"
+Score_team1 = 1
+Score_team2 = 7
+
+print(f'{team1}-{team2} : {Score_team1}-{Score_team2}')
--- 101HelloWorld.py
+++ 101HelloWorld.py
@@ -1,1 +1,4 @@
 print("Hello World!6767")
+a = int(input())
+if a == 67:
+    print('You Are Nigger')
